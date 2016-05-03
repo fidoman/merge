826076fb68844f809fce9a1a1a83b5908c332b72
sergey <sergey@fidoman.ru>
2016-05-03 14:31:06 +0300

    Upload

---
+++ merge.py
@@ -0,0 +1,394 @@
+#!/usr/bin/python3 
+
+import apsw
+from tkinter import *
+import time
+import ast
+import os
+
+INIT=True
+
+if INIT:
+  os.unlink("burbeer.sqlite")
+
+conn = apsw.Connection("burbeer.sqlite")
+conn.cursor().execute("PRAGMA synchronous=off")
+
+
+conn.cursor().execute("create table if not exists sources(srcid, srcfile, prefix)")
+conn.cursor().execute("create unique index if not exists srcs_index ON sources (srcid)")
+if INIT:
+  conn.cursor().execute("insert into sources values (1, 'pilot.dat', '')")
+  conn.cursor().execute("insert into sources values (2, 'spectorg_q.dat', 'S-')")
+
+
+conn.cursor().execute("create table if not exists src_data(src_exists, srcid, goodid, xid, name, "
+      "img, price, pack, bulk, year, code, barcode, descr, manuf, avail, grp, net_weight, volume)")
+conn.cursor().execute("create unique index if not exists src_index ON src_data (srcid, xid)")
+
+conn.cursor().execute("create table if not exists goods(goodid INTEGER PRIMARY KEY AUTOINCREMENT, name, description, volume, manufacturer, year, code, pic)")
+
+# events: 
+#   attribute change
+#   position addition
+#   position removal
+
+# addition
+#   try to match with existing good
+#   update availability if ok
+#   else create new good
+
+# removal
+#   if not last update availability
+#   if last set availability to zero
+
+# attribute change
+#   mark good for review
+
+# OR do not store price and availability, fetch on export from src_data table
+
+
+root = Tk()
+
+cs = conn.cursor()
+
+def new_srcrec(conn, gui, src_record):
+  "returns goodid if assignment succeful"
+  1/0
+
+print("SOURCES ANALYZIS")
+# no database changes during analysis
+# changes must be reviewed and then processed
+
+#1 mark all src_data records as unverified
+cs.execute("update src_data set src_exists=?", (False,))
+
+#2 walk new sources
+#21 mark record as verified if they are present in source
+#211 mark matching object for review if data changed
+#22 if source record is new start object add/assign to existing procedure
+
+changes = {}
+new_recs = []
+deletions = []
+
+sources = list(cs.execute("select * from sources"))
+for srcid, srcfile, prefix in sources:
+  for l in open(srcfile):
+    xid, name, img, price, pack,bulk, year,code,barcode,descr,manuf,avail,group,net_weight = \
+      ast.literal_eval(l.strip())
+    volume=None # currently no source contains this field
+    #print(xid, name)
+    existing_data = list(cs.execute("select * from src_data where srcid=? and xid=?", (srcid, xid)))
+    if existing_data:
+      #print("src", srcid, "same record", xid)
+      cs.execute("update src_data set src_exists=? where srcid=? and xid=?", (True,srcid, xid))
+      #..compare
+      db_name, db_img, db_price, db_pack, db_bulk, db_year, db_code, db_barcode, db_descr,\
+        db_manuf, db_avail, db_group, db_net_weight, db_volume = \
+        list(cs.execute("select name, img, price, pack,bulk, year,code,barcode,descr,manuf,avail,grp,net_weight,volume "
+        "from src_data where srcid=? and xid=?", (srcid, xid)))[0]
+      #.. if manuf or year or code differs - drop relation as its different good (except for None --> value)
+      if db_manuf and db_manuf!=manuf or db_year and db_year!=year or db_code and db_code!=code:
+        print("record", srcid, xid, "identication changed, dropping")
+        # delete relation to good - recreate record
+        deletions.append([srcid, xid])
+        new_recs.append([srcid, xid, name, img, price, pack,bulk, year,code,barcode,descr,manuf,avail,group,net_weight,volume])
+
+      elif name!=db_name or img!=db_img or pack!=db_pack or bulk!=db_bulk or barcode!=db_barcode or \
+        descr!=db_descr or group!=db_group or net_weight!=db_net_weight or volume!=db_volume:
+        print("record", srcid, xid, "updated")
+        print(db_name, db_img, db_pack, db_bulk, db_barcode, db_descr, db_group, db_net_weight, db_volume)
+        print(name, img, pack, bulk, barcode, descr, group, net_weight, volume)
+
+        changes[(srcid, xid)]=[
+          (db_name, db_img, db_pack, db_bulk, db_barcode, db_descr, db_group, db_net_weight, db_volume), 
+          (name, img, pack, bulk, barcode, descr, group, net_weight, volume)]
+      else:
+#        print("record", srcid, xid, "no changes")
+        pass
+    else:
+      print("src", srcid, "new record", xid)
+      new_recs.append([srcid, xid, name, img, price, pack,bulk, year,code,barcode,descr,manuf,avail,group,net_weight,volume])
+
+  print(srcfile, "done")
+
+deletions = list(cs.execute("select srcid, xid from src_data where src_exists=?", (False,)))
+
+print("new records:", len(new_recs))
+print("updated:", len(changes))
+print("deleted:", len(deletions))
+
+print("END SOURCES ANALYZIS")
+
+# deletions - drop from sources
+# new records - match with goods (with code, name, or manual selection)
+# updated (and newly matched) - mark for review
+
+
+# --- INIT ---
+# INITIALIZATION
+# 1) load first source
+# 2) create good for each its record
+# 3) update goods with data from translations.ods
+# 4) second source process with manual matching
+if INIT:
+  print("reading translations.dat...")
+  trans = {}
+  for l in open("translations.dat"):
+    xid, name, descr, vol = ast.literal_eval(l.strip())
+    trans[xid] = (name, descr, vol)
+
+  m={}
+  for srcid, xid, name, img, price, pack,bulk, year,code,barcode,descr,manuf,avail,group,net_weight,volume in new_recs:
+    if srcid==1:
+      #print("adding xid", xid)
+      #..create good, get data from translations
+      if xid in trans:
+        tr=trans[xid]
+        g_name=tr[0] or name
+        g_descr=tr[1] or descr
+        g_volume=tr[2] or volume
+        print(xid, "is known, adding as good")
+        cs.execute("insert into goods values (NULL, ?, ?, ?, ?, ?, ?, ?)", (g_name, g_descr, g_volume, manuf, year, code, img))
+        goodid = conn.last_insert_rowid()
+        cs.execute("insert into src_data values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+          (True, srcid, goodid, xid, name, img, price, pack,bulk,year,code,barcode,descr,manuf,avail,group,net_weight,volume))
+      else:
+        print(xid, "not in translations, skipping")
+
+    else:
+      print(srcid, name, manuf, code)
+      if code:
+        e=list(cs.execute("select * from goods where code=?", (code,)))
+        if e:
+          #m[(manuf, e[0][4])]=m.get((manuf, e[0][4]), 0) + 1
+          m.setdefault((manuf, e[0][4]), []).append(code)
+
+  open("m","w").write(repr(m))
+# --- END INIT ---
+
+
+#3 set availability in unverified to 0 -- generate on export
+#4 review updated objects
+#41 reset updated flag after review
+
+# -----------------------
+# process deletions
+for srcid, xid in deletions:
+  print("deleting",srcid,xid)
+  cs.execute("delete from src_data where srcid=? and xid=?", (srcid, xid))
+  # no more actions as unsources good are not exported due to no source with non-zero availability
+
+# new records
+#for srcid, xid, name, img, price, pack,bulk, year,code,barcode,descr,manuf,avail,group,net_weight in new_recs:
+#  print("new_rec", srcid, xid)
+#exit()
+
+# -----------------------
+
+
+#main window
+
+#list of goods with filter/search
+#  + filter for updated
+#for selected good the following is shown:
+#  pic
+#  attributes
+#  source data
+#  old source data (for updated)
+
+#  delete good button
+#  remove source button for each assigned source
+
+#tabs for all sources:
+#  list of unassigned records
+#  button "assign" to assign to currently selected good if has not already assigned record from this source
+
+
+fgoods = LabelFrame(root, text="Товары")
+fgoods.grid(row=0, column=0)
+
+
+
+goodslist = Frame(fgoods)
+goodslist.grid(row=0, column=0)
+
+
+goodfilter = Entry(goodslist)
+goodfilter.grid(row=0, sticky=N)
+
+gfapply = Button(goodslist, text="Фильтр")
+gfapply.grid(row=1)
+
+gfchanged = Button(goodslist, text="Изменённые")
+gfchanged.grid(row=2)
+
+goods = Listbox(goodslist, exportselection=0)
+goods.grid(row=3, sticky=S+N)
+
+for gi, gn in cs.execute("select goodid, name from goods"):
+  goods.insert(END, "%d %s"%(gi,gn))
+
+delgood = Button(goodslist, text="удалить товар")
+delgood.grid(row=4)
+
+
+
+goodinfo = Frame(fgoods)
+goodinfo.grid(row=0, column=1)
+
+pic = Canvas(goodinfo, width=256, height=256)
+pic.grid(row=0, column=2, rowspan=8)
+
+name = Entry(goodinfo, width=40)
+name.grid(row=0, column=0, columnspan=2)
+
+descr = Entry(goodinfo, width=40)
+descr.grid(row=1, column=0, columnspan=2)
+
+volume = Entry(goodinfo, width=40)
+volume.grid(row=2, column=0, columnspan=2)
+
+manuf = Entry(goodinfo, width=40)
+manuf.grid(row=3, column=0, columnspan=2)
+
+year = Entry(goodinfo, width=40)
+year.grid(row=4, column=0, columnspan=2)
+
+code = Entry(goodinfo, width=40)
+code.grid(row=5, column=0, columnspan=2)
+
+update_good = Button(goodinfo, text="Обновить поля")
+update_good.grid(row=6, column=0, columnspan=2)
+
+# list of connected sources
+goodsources = Listbox(goodinfo, exportselection=0)
+goodsources.grid(row=7, column=0)
+
+# source data in text form, available for copying
+# if source if changed, modification is shown here as 
+# NEW: and OLD: separate lines
+goodsourcedata = Text(goodinfo, width=30, height=10)
+goodsourcedata.grid(row=7, column=1)
+
+goodinfofields = [name, descr, volume, manuf, year, code, pic, goodsources, goodsourcedata]
+
+def refresher(goods, i):
+  sel=goods.curselection()
+  if sel!=goods.oldselection:
+    i[0].delete(0, END)
+    i[1].delete(0, END)
+    i[2].delete(0, END)
+    i[3].delete(0, END)
+    i[4].delete(0, END)
+    i[5].delete(0, END)
+    i[7].delete(0, END)
+    i[7].oldsel=i[7].curselection()
+    i[8].delete("0.0", END)
+
+    if sel:
+      goodid=goods.get(sel[0]).split(' ',1)[0]
+      print(goodid)
+      gname, gdescription, gvolume, gmanufacturer, gyear, gcode, gpic = \
+        list(cs.execute("select name, description, volume, manufacturer, year, code, pic from goods where goodid=?", (goodid,)))[0]
+      i[0].insert(0, gname or '')
+      i[1].insert(0, gdescription or '')
+      i[2].insert(0, gvolume or '')
+      i[3].insert(0, gmanufacturer or '')
+      i[4].insert(0, gyear or '')
+      i[5].insert(0, gcode or '')
+      i[6].create_text((128,128), text='figa.jpg')
+      print("selected goodid=", repr(goodid))
+      gsrcs=list(cs.execute("select srcid, xid "
+        #"src_exists, srcid, goodid, xid, name, "
+        #"img, price, pack, bulk, year, code, barcode, descr, manuf, avail, grp, net_weight, volume"
+        " from src_data where goodid=?",
+        (int(goodid),)))
+      for gsrcid, gxid in gsrcs:
+        gsrcname=list(cs.execute("select srcfile from sources where srcid=?", (int(gsrcid),)))[0][0]
+        i[7].insert(END, "source %s %s"%(gsrcid, gsrcname))
+
+      i[7].oldsel=i[7].curselection()
+
+    goods.oldselection=sel
+    
+  else:
+    # just check selection in sources listbox
+    if i[7].curselection()!=i[7].oldsel:
+      print(i[7].curselection(), i[7].oldsel)
+      i[8].insert(END, "line1\n")
+      i[8].insert(END, "line2\n")
+      i[7].oldsel=i[7].curselection()
+    
+  goods.after(200, lambda: refresher(goods, i))
+
+def good_updater(goods, i):
+  sel=goods.curselection()
+  if sel:
+    goodid=goods.get(sel[0]).split(' ',1)[0]
+    cs.execute("update goods set name=?, description=?, volume=?, manufacturer=?, year=?, code=? "
+      "where goodid=?", (i[0].get(), i[1].get(), i[2].get(), i[3].get(), i[4].get(), i[5].get(), goodid))
+    print(goodid, "updated")
+
+update_good.config(command=lambda: good_updater(goods, goodinfofields))
+
+
+goods.oldselection=None
+goods.after(1000, lambda: refresher(goods, goodinfofields))
+
+
+unbindsrc = Button(goodinfo, text="отвязать выбранный источник")
+unbindsrc.grid(row=8, column=0, columnspan=2)
+
+# --- sources
+
+fsuppliers = LabelFrame(root, text="Прайслисты")
+fsuppliers.grid(row=0, column=1)
+
+sources = Listbox(fsuppliers, height=3)
+sources.pack()
+
+srcrecs = Listbox(fsuppliers)
+srcrecs.pack()
+
+searchgood = Button(fsuppliers, text="найти подходящие товары")
+searchgood.pack()
+
+assignrec = Button(fsuppliers, text="привязать к выбранному товару")
+assignrec.pack()
+
+#w = Label(root, text="preparing...")
+#w.pack()
+root.mainloop()
+
+# 'attribute changed' window
+# Good - frame. attributes, pic
+# sources - table. chnaged attribute will be marked red
+# buttons:
+# Accept
+#   edits of good are saved
+#   source data is updated
+# Later
+#   edits of good are saved
+#   source data is NOT updates
+# Quit
+#   quit immediately
+
+# 'new source' window
+# merge with existing good
+# data from source is shown
+# list of goods.
+# 1) filtered. only matching with source data
+# 2) full list. search, filter
+# preview selected good
+# buttons:
+#  merge - merge with selected good
+#  new - new good (goto 'new good' window)
+
+
+# 'new good' window
+# is the same
+# good data is prefilled by auto-generated data from source
+
+conn.close(True)
---
+++ read_ods.py
@@ -0,0 +1,393 @@
+#!/usr/bin/python3
+
+import zipfile
+import xml.sax
+
+import os
+import urllib
+
+import sys
+import ast
+import re
+
+class TableGrouper:
+  def __init__(self):
+    self.rows=[]
+    self.dump = open("tdump", "w")
+    self.limit=100
+    self.groups=[]
+
+  def __del__(self):
+    if self.dump:
+      self.dump.close()
+    pass
+
+  def row(self, r, rlinks):
+    """ add row to table 
+        second field for all row hyperlinks"""
+#    print("name>", r[2])
+#    print("quantity>", r[3])
+#    print("code_seller>", r[4])
+#    print("code_manuf>", r[5])
+    self.rows.append([self.groups.copy(), r, rlinks])
+
+#    self.dump.write("|".join(self.groups)+"\t")
+#    self.dump.write("\t".join(map(lambda x: "|".join(x), r)))
+    #self.dump.write(repr(self.rows[-1]))
+    #self.dump.write("\n")
+    #self.limit -= 1
+    if not self.limit:
+      print("LIMIT!")
+      exit()
+    pass
+
+  def group_begin(self):
+    """ get row description from last row.
+        make it added to all rows in the group """
+    if self.dump: self.dump.write("group begin\n")
+    g_row = self.rows.pop()
+    if self.dump: self.dump.write(repr(g_row)+"\n")
+    g_cells = g_row[1]
+    if g_cells:
+      grp = ''.join(g_cells[1])
+    else:
+      grp = ''
+    self.groups.append(grp)
+    pass
+
+  def group_end(self):
+    """ drop group from the addition to rows """
+    if self.dump: self.dump.write("group end\n")
+    self.groups.pop()
+    pass
+
+class Cell:
+  def __init__(self):
+    self.data=[]
+    self.repeat=1
+    self.links=[]
+
+class ODSDecoder(xml.sax.ContentHandler):
+  def __init__(self, tablebuilder):
+    self.t = tablebuilder
+
+  def startDocument(self):
+    self.level = 0
+    #self.table = []
+    self.row = []
+    self.cell = None
+    self.skip = 0
+    self.skiptag = None
+    #self.group_enter = False
+    #self.groupstack = []
+
+  def endDocument(self):
+    pass
+
+  def startElement(self, name, attrs):
+    if self.skip:
+      if name == self.skiptag:
+        self.skip+=1
+      return
+#    print(" "*self.level, name)
+#    for k in attrs.keys():
+#      print(" "*self.level,'-',k,'=',attrs[k])
+    self.level+=1
+    if name=="office:automatic-styles":
+      self.skip=1
+      self.skiptag=name
+    if name=="table:table-cell":
+      self.cell=Cell()
+      self.cell.repeat = int(attrs.get("table:number-columns-repeated", "1"))
+    if name=="table:table-row-group":
+      #print("-"*20)
+      #self.group_enter = True
+      self.t.group_begin()
+    if name=="text:a":
+      #print(attrs.keys(), attrs.values())
+      self.cell.links.append(attrs['xlink:href'])
+
+  def characters(self, content):
+    if not self.skip:
+      #print(" "*self.level, content)
+      self.cell.data.append(content)
+
+  def endElement(self, name):
+    if self.skip:
+      if name==self.skiptag:
+        self.skip-=1
+    if not self.skip: 
+      self.level-=1
+      if name=="table:table-cell":
+        self.row.append(self.cell)
+        self.cell = None
+      if name=="table:table-row":
+        """ drop empty trailing cells """
+        while self.row and self.row[-1].data == []:
+          self.row.pop()
+        r=[]
+        rlinks=[]
+        for c in self.row:
+          r.extend([c.data]*c.repeat)
+          rlinks+=c.links
+        self.t.row(r, rlinks)
+        self.row = []
+      if name=="table:table-row-group":
+        self.t.group_end() 
+
+# *** SPECTORG ***
+
+def read_spectorg_quantities():
+  z=zipfile.ZipFile(r"spectorg_q.ods")
+  f=z.open("content.xml")
+  parser = xml.sax.make_parser()
+  t = TableGrouper()
+  o = ODSDecoder(t)
+  parser.setContentHandler(o)
+  parser.parse(f)
+  f.close()
+  z.close()
+
+  descf = open("spectorg.dat")
+  desc = {}
+  for d in descf:
+    xid,name,code,pack1,pack2,img_f,material,manuf,grouping = ast.literal_eval(d.strip())
+    desc[xid]=[name,code,pack1,pack2,img_f,material,manuf,grouping]
+  descf.close()
+
+  of = open("spectorg_q.dat", "w")
+  first=True
+  for grouping, cells, hrefs in t.rows:
+    if len(cells)<8:
+      continue
+    if first:
+      first=False
+      continue
+    
+    xid = ''.join(cells[0])
+    price = ''.join(cells[5])
+    avail = ''.join(cells[7])
+    if xid in desc:
+      name,code,pack1,pack2,img_f,material,manuf,grouping = desc[xid]
+      of.write(repr([xid, name, img_f, price, pack1, pack2, None, code, None, material, manuf, avail, '/'.join(grouping), None]) + "\n")
+    else:
+      print("no info for", xid)
+  of.close()
+
+
+def read_spectorg():
+  z=zipfile.ZipFile(r"spectorg.ods")
+  f=z.open("content.xml")
+  parser = xml.sax.make_parser()
+  t = TableGrouper()
+  o = ODSDecoder(t)
+  parser.setContentHandler(o)
+  parser.parse(f)
+  f.close()
+  z.close()
+
+  imgs = {}
+  for f in os.listdir("spectorg_images"):
+    imgs[os.path.splitext(f)[0].lower()] = f
+    # use lowercase name without extension as key to find file
+
+  #material=set()
+  #manuf=set()
+
+  f = open("spectorg.dat", "w")
+  #f.write(repr(['Группа', 'Пусто1', 'Наименование', 'Идентификатор', 'Артикул', 'Упаковка1', 'Упаковка2', 'Изображение', 'Материал', 'Производитель', 'Цена'])+'\n')
+  for grouping, cells, hrefs in t.rows:
+    if not grouping:
+      continue
+  #f.write(u"\t".join(map(lambda x: u"|".join(x), r)))
+#    f.write(u"\u200b".join(c)+u"\t")
+#    if len(u"\u200b".join(c)+u"\t")>200:
+#      exit()
+
+    name=''.join(cells[1]).strip()
+    xid=''.join(cells[2]).strip()
+    code=''.join(cells[3]).strip()
+    pack1=''.join(cells[4]).strip()
+    pack2=''.join(cells[5]).strip()
+
+    if cells[6]:
+      img_id = ''.join(cells[6]).lower().strip()
+      img_f=imgs.get(img_id)
+
+    material=''.join(cells[7]).strip()
+    manuf=''.join(cells[8]).strip()
+    f.write(repr([xid,name,code,pack1,pack2,img_f,material,manuf,grouping])+"\n")
+
+  f.close()
+
+#  open("material", "w").write('\n'.join(list(material)))
+#  open("manuf", "w").write('\n'.join(list(manuf)))
+
+
+
+# *** PILOT MS ***
+
+def de_esc(s):
+  if not s:
+    return ""
+  if s[0]=='"' and s[-1]=='"':
+    return s[1:-1].replace('""', '"')
+  else:
+    return s.strip()
+
+def split_tsv(s):
+  return map(de_esc, s.rstrip("\n").split("\t"))
+
+
+def read_pilot_groups():
+  print("PILOT: Reading groups")
+  groups={}
+  for l in open("pilot_groups.tsv"):
+    g_id, g_name, g_file, g_export, g_displayname, g_manuffilter = split_tsv(l)
+    groups[g_name] = {"displayname": g_displayname, "manuffilter": g_manuffilter, "export": True if g_export=="True" else False}
+    if g_file:
+      g_fn="pilot_groups/"+g_file[:-4]+".ods"
+      print("Reading", g_fn)
+      data = {}
+#      try:
+      if True:
+        z=zipfile.ZipFile(g_fn)
+        f=z.open("content.xml")
+        parser = xml.sax.make_parser()
+        t = TableGrouper()
+        o = ODSDecoder(t)
+        parser.setContentHandler(o)
+        parser.parse(f)
+        f.close()
+        z.close()
+        g_info = groups[g_name]["data"] = {}
+        for grouping, cells, hrefs in t.rows:
+          if len(cells)<14:
+            continue
+          g_info[''.join(cells[0]).strip()] = {"net": ''.join(cells[13])}
+
+#      except:
+#        print("Failed:", g_fn)
+#        continue
+  of = open("pilot_groups.dat", "w")
+  of.write(repr(groups))
+  of.close()
+
+
+pilot_img_prefix="http://www.pilotms.ru/bin.aspx?ID="
+pilot_img_dir="pilot_images"
+def pilot_img(url):
+  if url.startswith(pilog_img_prefix):
+        uid=url[len(pilog_img_prefix):]
+        fn=os.path.join(pilot_img_dir, uid+".jpg")
+        if not os.path.exists(fn):
+          print("downloading", url, "->", fn)
+          #q=urllib.request.urlopen(url)
+          try:
+            q=urllib.urlopen(url)
+            #data=q.readall()
+            data=q.read()
+            f=open(fn, "wb")
+            f.write(data)
+            f.close()
+          except:
+            print("failed")
+            faillog = open("pilog_img_fail", "a")
+            faillog.write("%s\t%s\n"%(xid, url))
+            faillog.close()
+  else:
+        print ("bad url", url, "for", name)
+
+def read_pilot():
+  groups = ast.literal_eval(open("pilot_groups.dat").read())
+
+  z=zipfile.ZipFile(r"pilot.ods")
+  f=z.open("content.xml")
+  parser = xml.sax.make_parser()
+  t = TableGrouper()
+  o = ODSDecoder(t)
+  parser.setContentHandler(o)
+  parser.parse(f)
+  f.close()
+  z.close()
+
+  of=open("pilot.dat", "w")
+  first = True
+  for grouping, cells, hrefs in t.rows:
+    if len(cells)<12:
+      continue
+    if first:
+      first = False
+      continue
+
+    url=hrefs[0] if hrefs else None
+    xid=''.join(cells[0])
+    name=''.join(cells[1])
+    price=''.join(cells[2])
+    pack=''.join(cells[3])
+    bulk=''.join(cells[4])
+    code=''.join(cells[5])
+    barcode=''.join(cells[6])
+    descr=''.join(cells[7])
+    manuf=''.join(cells[8])
+    avail=''.join(cells[9])
+    group=''.join(cells[11])
+    
+    g_info = groups.get(group)
+    if g_info is None:
+      print("group is unknown", group)
+    group = g_info["displayname"] or group
+    manuffilter = g_info["manuffilter"]
+    export = g_info["export"]
+    data = g_info.get("data")
+
+    if manuffilter:
+      print(manuf, manuffilter, re.match(manuffilter, manuf))
+      if not re.match(manuffilter, manuf):
+        print(" - skip", name)
+        continue
+
+    net_weight = None
+    if data:
+      x = data.get(xid)
+      if x:
+        net_weight=x.get("net")
+
+    if export:
+      of.write(repr([xid,name,url,price,pack,bulk,None,code,barcode,descr,manuf,avail,group,net_weight])+"\n")
+    else:
+      print("skip", name)
+  of.close()
+
+# *  *  *  *  *
+
+if False:
+  z=zipfile.ZipFile("translations.ods")
+  f=z.open("content.xml")
+  parser = xml.sax.make_parser()
+  t = TableGrouper()
+  o = ODSDecoder(t)
+  parser.setContentHandler(o)
+  parser.parse(f)
+  f.close()
+  z.close()
+  of=open("translations.dat", "w")
+  for r in t.rows:
+    text = r[1]
+    if not text:
+      continue
+    xid=''.join(text[0])
+    name=''.join(text[2])
+    vol=''.join(text[3])
+    if len(text)>7:
+      descr=''.join(text[7])
+    else:
+      descr=None
+    of.write(repr([xid,name,descr,vol])+'\n')
+  of.close()
+  exit()
+
+read_spectorg()
+read_spectorg_quantities()
+read_pilot_groups()
+read_pilot()
